1710132464bf3fcf2d4e729e12d48a408584b7a4
tschwenke <tschwenke7@gmail.com>
2021-09-13 15:08:00 +1000

    Implement dynamic form validation messages.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -14,20 +14,32 @@ export class FormComponent extends AccordionComponent implements OnInit  {
 
   studyLocationVisible: boolean = false;
 
+  //show/hide study location field when isStudent radio buttons are clicked.
   selectIsStudent(event: any){
     if(event.target.value == "yes"){
       this.studyLocationVisible = true;
+      //enable required validator
+      this.studyLocation?.setValidators(Validators.required);
+      this.studyLocation?.updateValueAndValidity();
     }
     else {
       this.studyLocationVisible = false;
+      this.studyLocation?.clearValidators();
+      this.studyLocation?.updateValueAndValidity();
     }
   }
 
   onSubmit() {
+    //mark all fields as touched to trigger 'required' validation messages
+    this.userForm.markAllAsTouched();
+
+
+
     //block submission of form if validation has failed
     if(!this.formResponsesService.userForm.valid) {
       this.submissionMessage = "Error - One or more fields of this form need your attention before you can submit.";
     }
+    //if form was valid, send response to db
     else{
       let formContents = this.formResponsesService.userForm.value;
       let data: FormResponse = {
@@ -47,7 +59,6 @@ export class FormComponent extends AccordionComponent implements OnInit  {
       .then(res => {
         this.submissionMessage = "Form submitted successfully. Thanks for getting in touch!";
         this.formResponsesService.userForm.reset();
-        console.warn(data);
       })
       //display error if db couldn't be reached
       .catch(err => {
@@ -68,4 +79,16 @@ export class FormComponent extends AccordionComponent implements OnInit  {
   ngOnInit() {
     super.ngOnInit();
   }
+
+  get firstName() { return this.formResponsesService.userForm.get('firstName')}
+
+  get lastName() { return this.formResponsesService.userForm.get('lastName')}
+
+  get email() { return this.formResponsesService.userForm.get('email')}
+
+  get isStudent() { return this.formResponsesService.userForm.get('isStudent')}
+
+  get studyLocation() { return this.formResponsesService.userForm.get('studyLocation')}
+
+  get userForm() { return this.formResponsesService.userForm }
 }
