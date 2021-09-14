ec16023d63e9be2c8e2d4fa31b4839b169301962
tschwenke <tschwenke7@gmail.com>
2021-09-14 10:00:36 +1000

    Reimplement html structure using BootStrap classes

--- src/app/accordion.component.ts
+++ src/app/accordion.component.ts
@@ -8,10 +8,13 @@
  *           identify this component's accordion cookie
  * - implement 'OnInit' and call 'super.onInit()' within that method.
  * - constructor which includes a "CookieService" parameter which is passed in as 
- *      an argument to a "super(cookieService, ...)" call in the constructor. 
+ *      an argument to a "super(cookieService, ...)" call in the constructor.
+ * - in template:
+ *      - to button of class "accordion-button", add: [ngClass]="{'collapsed' : !accordionOpen}" [attr.aria-expanded]="accordionOpen"  
+ *      - to div that has class "accordion-collpase", add: [ngClass]="{'show' : accordionOpen}" 
  */
 import { StringMapWithRename } from '@angular/compiler/src/compiler_facade_interface';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Inject } from '@angular/core';
 
@@ -23,7 +26,7 @@ export class AccordionComponent implements OnInit {
 
 
   //set this from cookie on page load
-  private accordionOpen: boolean = true;
+  accordionOpen: boolean = true;
 
   private cookieName: string;
 
@@ -32,14 +35,12 @@ export class AccordionComponent implements OnInit {
       this.cookieName = 'accordion-open-' + cookieSuffix;
   }
 
-  ngOnInit() {
+  ngOnInit(){
     //set accordion's initial status based on cookie value, if available
     let cookieValue = this.cookieService.get(this.cookieName);
-    
     //if cookie found and open status is set to false, close accordion
     if(cookieValue == "false"){
         this.accordionOpen = false;
-        //todo - close accordion 
     }
     //if cookie not found or value is true/not false, open accordion
     else {
@@ -51,5 +52,6 @@ export class AccordionComponent implements OnInit {
     //flip accordion flag and update cookie
     this.accordionOpen = !this.accordionOpen;
     this.cookieService.set(this.cookieName, String(this.accordionOpen), 30);
+    console.warn(this.accordionOpen);
   }
 }
